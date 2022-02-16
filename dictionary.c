c4637fd7c1a970c34f4c936c485fe5376eefe4d0
braelan.creswell <braelan.creswell@maine.edu>
2022-02-16 19:34:39 +0000

    Organized dictionary.c

---
+++ dictionary.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *dictionary_init() {
+
+}
+
+char *dictionary_add(char *dictobj, char *key, char *value) {
+
+}
+
+char *dictionary_value(char *dictobj, char *key) {
+
+}
+
+char *dictionary_remove(char *dictobj, char *key) {
+
+}
+
+char *dictionary_destroy(char *dictobj) {
+
+}
+
+void dictionary_print(char *dictobj) {
+
+}
