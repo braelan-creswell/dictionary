f791aca1520f13e3861b7b102bcf9c292b5ad9f4
braelan.creswell <braelan.creswell@maine.edu>
2022-02-17 06:28:37 +0000

    Wrote init function

--- dictionary.c
+++ dictionary.c
@@ -1,27 +1,42 @@
 //dictionary.c
 #include <stdio.h>
 #include <stdlib.h>
+#include "dictionary.h"
+#include <errno.h>
 
-char *dictionary_init() {
-    
+//Initialize dictionary object
+struct *dict dictionary_init() { 
+    //Initialize first dictionary entry
+    struct dict *first = (struct dict*)malloc(sizeof(struct dict));
+    if(first == NULL) {
+        fprintf(stderr, "Malloc Error");
+        return NULL;
+    }
+    //Pass initial (empty) values to first object
+    first->key   = '';
+    first->value = '';
+    first->last  = NULL;
+    first->next  = NULL;
+
+    return first;
 }
 
-char *dictionary_add(char *dictobj, char *key, char *value) {
+struct *dict dictionary_add(struct dict *first, char *key, char *value) {
 
 }
 
-char *dictionary_value(char *dictobj, char *key) {
+struct *dict dictionary_value(struct dict *first,, char *key) {
 
 }
 
-char *dictionary_remove(char *dictobj, char *key) {
+struct *dict dictionary_remove(struct dict *first,, char *key) {
 
 }
 
-char *dictionary_destroy(char *dictobj) {
+struct *dict dictionary_destroy(struct dict *first,) {
 
 }
 
-void dictionary_print(char *dictobj) {
+void dictionary_print(struct dict *first,) {
 
 }
